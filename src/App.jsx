00b8dbed74fb01ebca31c55949e48512ed86698f
Robert P <porobertdev@gmail.com>
2024-12-11 15:52:54 +0200

    Create `Greeting` component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Greeting from './components/Greeting';
 import './App.css'
 
 function App() {
@@ -16,7 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <Greeting></Greeting>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
---
+++ src/components/Greeting.jsx
@@ -0,0 +1,5 @@
+function Greeting () {
+    return <h1>Greeting from Vite + React :)</h1>;
+};
+
+export default Greeting;
