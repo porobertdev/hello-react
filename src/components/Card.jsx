145b104151748759f1ac184660b5862d22a95e78
Robert P <porobertdev@gmail.com>
2024-12-11 16:14:12 +0200

    Break `App.js` into smaller components

---
+++ src/components/Card.jsx
@@ -0,0 +1,14 @@
+import Button from "./Button";
+
+function Card() {
+    return (
+        <div className="card">
+        <Button></Button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+    )
+};
+
+export default Card;
